86090f8267fd2fff5748f15eb1b3d755db7a2626
Igor Miranda <igorpcmiranda@gmail.com>
2024-11-14 23:51:38 +0000

    test

--- src/main/java/com/devigor/zilla_order/controller/ProductController.java
+++ src/main/java/com/devigor/zilla_order/controller/ProductController.java
@@ -18,5 +18,6 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
+
     }
 }
